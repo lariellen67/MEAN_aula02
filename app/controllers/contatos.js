08485b34721a6af08e139335dfb975db4e3e1ff3
Larissa Ellen Gomes Marques <larissa.marques@.stitdata.com>
2020-08-14 11:23:13 -0300

    update-changes

--- app/controllers/contatos.js
+++ app/controllers/contatos.js
@@ -2,19 +2,18 @@ var contacts = [
   { _id: 1, nome: 'Larissa Marques', email: 'larissa.marques@ifsp.edu.br' },
   { _id: 2, nome: 'Rodrigo Inoue', email: 'rodrigo.inoue@ifsp.edu.br' },
   { _id: 3, nome: 'Gabriela Mota', email: 'gabriela.mota@ifsp.edu.br' },
-  { _id: 4, nome: 'Daniel Samarone', email: 'daniel.samarone@ifsp.edu.br' }
+  { _id: 4, nome: 'Daniel Samarone', email: 'daniel.samarone@ifsp.edu.br' },
 ];
 
-module.exports = function() {
+module.exports = function () {
   var controller = {};
-  controller.listContacts = function(req, res) {
-    console.log(this.listContacts, 'to aki');
+  controller.listContacts = function (req, res) {
     res.json(contacts);
   };
-  controller.getContact = function(req, res) {
+  controller.getContact = function (req, res) {
     console.log('Contato selecionado: ' + req.params.id);
     var contactId = req.params.id;
-    var contact = contacts.filter(function(contact) {
+    var contact = contacts.filter(function (contact) {
       return contact._id == contactId;
     })[0];
     contact
